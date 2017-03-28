5890b68da3ca6f0782499b5b03bae9553dbde000
Daniil Yarancev <tiberiumk12@gmail.com>
2017-03-28 12:10:06 +0300

    Упс

--- src/plugins/example.nim
+++ src/plugins/example.nim
@@ -2,4 +2,4 @@ include base
 
 proc call*(api: VKAPI, msg: Message) =
   let argsStr = msg.cmd.arguments.join(", ")
-  api.answer(msg, "12312312312" & argsStr)
+  api.answer(msg, "Это тестовая команда. Аргументы - " & argsStr)
