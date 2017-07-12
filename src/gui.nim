52f840adba828a36b912e89e844ddedb28a56254
Daniil Yarancev <TiberiumN@users.noreply.github.com>
2017-07-12 21:32:47 +0300

    Упростил gui с новыми изменениями genui

--- src/gui.nim
+++ src/gui.nim
@@ -1,23 +1,15 @@
-{.experimental.}
 import nigui, asyncdispatch
 export nigui
-
 app.init()
 
 genui:
-  {var tempwin = @result} Window[width = 800, height = 600]("Nickel - бот для ВКонтакте"):
-    LayoutContainer(Layout_Vertical):
-      {var temptext = @result} TextArea()
-
-var window* = tempwin
-var guiLog* = temptext
+  {(var window* = @result)} Window[width = 800, height = 600]("Nickel - бот для ВКонтакте"):
+    LayoutContainer(LayoutVertical):
+      {(var guiLog* = @result)} TextArea()
 
 proc runBot(event: TimerEvent) =
-  # Если есть задачи, которые нужно обработать
   if hasPendingOperations():
-    # Даём 2мс на заканчивание/добавление новых асинхронных задач
-    # В эти 2мс GUI будет "заморожено" 
     poll(2)
-# Таймер, который каждые 25мс вызывает функцию runBot
+
 var timer = startRepeatingTimer(25, runBot)
 window.show()
